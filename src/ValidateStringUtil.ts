b57d06c246613bdd8945f73e377f3c6fcb2f005d
ndaira <nakadaira44@yahoo.co.jp>
2025-07-28 01:07:13 +0900

    add

--- src/ValidateStringUtil.ts
+++ src/ValidateStringUtil.ts
@@ -32,4 +32,47 @@ export class ValidateStringUtil {
         const extension = value.split(".").pop();
         return extensions.includes(extension ?? '');
     }
+
+    /**
+     * 値がメールアドレス形式であるかどうかを検証します
+     * Validates if the given value is in the format of an email address
+     * @param value - 検証する値, The value to be validated
+     * @returns {boolean} - 値がメールアドレス形式であるかどうか, Whether the value is in the format of an email address
+     */
+    public static isMail(value: any) {
+        if (typeof value !== 'string') {
+            return false;
+        }
+
+        const pattern = new RegExp('^[a-zA-Z0-9_%+-]+([.][a-zA-Z0-9_%+-]+)*@[a-zA-Z0-9]+([-.]?[a-zA-Z0-9]+)*\\.[a-zA-Z]{2,}$');
+        return pattern.test(value);
+    }
+
+    public static isBase64(value: any): value is string {
+        if (typeof value !== 'string') {
+            return false;
+        }
+
+        // Data URIの場合はBase64部分だけ抽出
+        if (value.startsWith('data:')) {
+            const parts = value.split(',');
+            if (parts.length !== 2) {
+                return false;
+            }
+            value = parts[1];
+        }
+
+        if (value.length % 4 !== 0) {
+            return false;
+        }
+        
+        // 基本的なbase64パターン
+        // 使用可能な文字
+        // ・ アルファベット（A-Z, a-z）
+        // ・ 数字（0-9）
+        // ・ +と/（基本文字）
+        // ・ =（パディング文字）
+        const base64Pattern = /^[A-Za-z0-9+/]*={0,2}$/;
+        return base64Pattern.test(value);
+    }
 }
