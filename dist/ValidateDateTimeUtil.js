38760e10b6e8fccac284a12d96d549349e6fa0d8
ndaira <nakadaira44@yahoo.co.jp>
2025-07-28 23:02:21 +0900

    add

--- dist/ValidateDateTimeUtil.js
+++ dist/ValidateDateTimeUtil.js
@@ -28,67 +28,67 @@ class ValidateDateTimeUtil {
             return true;
         }
     }
-    // /**
-    //  * Generates a Date object from a string.
-    //  * 文字列からDateオブジェクトを生成します。
-    //  * @param dateString A string representing the date and time (e.g., "2023-10-05 14:30:00")
-    //  * 日付と時間を表す文字列（例: "2023-10-05 14:30:00"）
-    //  * @returns Date object
-    //  * Dateオブジェクト
-    //  */
-    // static toDateFromString(dateString: string): Date {
-    //     const [datePart, timePart] = dateString.split(' ');
-    //     const [year, month, day] = datePart.split('-').map(Number);
-    //     let [hours, minutes, seconds] = [0, 0, 0];
-    //     if (timePart !== undefined) {
-    //         [hours, minutes, seconds] = timePart.split(':').map(Number);
-    //     }
-    //     return new Date(year, month - 1, day, hours, minutes, seconds);
-    // }
-    // /**
-    //  * Formats the specified date.
-    //  * 指定された日付をフォーマットします。
-    //  * @param date The date object to be formatted.
-    //  * フォーマットする対象の日付オブジェクト
-    //  * @param type A string specifying the type of format.
-    //  * フォーマットの種類を指定する文字列
-    //  * @returns A formatted date string.
-    //  * フォーマットされた日付文字列
-    //  */
-    // static toStringFromDate(date: Date, type: 'datetime' | 'date' | 'time'): string {
-    //     const year = date.getFullYear().toString().padStart(4, '0');
-    //     const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    //     const day = date.getDate().toString().padStart(2, '0');
-    //     const hour = date.getHours().toString().padStart(2, '0');
-    //     const minute = date.getMinutes().toString().padStart(2, '0');
-    //     const second = date.getSeconds().toString().padStart(2, '0');
-    //     switch (type) {
-    //         case 'datetime':
-    //             return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
-    //         case 'date':
-    //             return `${year}-${month}-${day}`;
-    //         case 'time':
-    //             return `${hour}:${minute}:${second}`;
-    //         default:
-    //             throw new Error('Invalid type');
-    //     }
-    // }
-    // /**
-    //  * Validates if the given value is in the format YYYY-MM-DD
-    //  * 与えられた値がYYYY-MM-DD形式であるかどうかを検証します
-    //  * @param value - The value to be validated, 検証する値
-    //  * @returns {boolean} - Whether the value is in the format YYYY-MM-DD, 値がYYYY-MM-DD形式であるかどうか
-    //  */
-    // static isYYYYMMDD(value: any) {
-    //     if (typeof value !== 'string') {
-    //         return false;
-    //     }
-    //     const pattern = new RegExp('^\\d{4}-\\d{2}-\\d{2}$');
-    //     if (pattern.test(value) === false) {
-    //         return false;
-    //     }
-    //     return this.isErrorDateTime(value) === false;
-    // }
+    /**
+     * Generates a Date object from a string.
+     * 文字列からDateオブジェクトを生成します。
+     * @param dateString A string representing the date and time (e.g., "2023-10-05 14:30:00")
+     * 日付と時間を表す文字列（例: "2023-10-05 14:30:00"）
+     * @returns Date object
+     * Dateオブジェクト
+     */
+    static toDateFromString(dateString) {
+        const [datePart, timePart] = dateString.split(' ');
+        const [year, month, day] = datePart.split('-').map(Number);
+        let [hours, minutes, seconds] = [0, 0, 0];
+        if (timePart !== undefined) {
+            [hours, minutes, seconds] = timePart.split(':').map(Number);
+        }
+        return new Date(year, month - 1, day, hours, minutes, seconds);
+    }
+    /**
+     * Formats the specified date.
+     * 指定された日付をフォーマットします。
+     * @param date The date object to be formatted.
+     * フォーマットする対象の日付オブジェクト
+     * @param type A string specifying the type of format.
+     * フォーマットの種類を指定する文字列
+     * @returns A formatted date string.
+     * フォーマットされた日付文字列
+     */
+    static toStringFromDate(date, type) {
+        const year = date.getFullYear().toString().padStart(4, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        const hour = date.getHours().toString().padStart(2, '0');
+        const minute = date.getMinutes().toString().padStart(2, '0');
+        const second = date.getSeconds().toString().padStart(2, '0');
+        switch (type) {
+            case 'datetime':
+                return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+            case 'date':
+                return `${year}-${month}-${day}`;
+            case 'time':
+                return `${hour}:${minute}:${second}`;
+            default:
+                throw new Error('Invalid type');
+        }
+    }
+    /**
+     * Validates if the given value is in the format YYYY-MM-DD
+     * 与えられた値がYYYY-MM-DD形式であるかどうかを検証します
+     * @param value - The value to be validated, 検証する値
+     * @returns {boolean} - Whether the value is in the format YYYY-MM-DD, 値がYYYY-MM-DD形式であるかどうか
+     */
+    static isYYYYMMDD(value) {
+        if (typeof value !== 'string') {
+            return false;
+        }
+        const pattern = new RegExp('^\\d{4}-\\d{2}-\\d{2}$');
+        if (pattern.test(value) === false) {
+            return false;
+        }
+        return this.isErrorDateTime(value) === false;
+    }
     /**
      * Validates if the given value is in the format YYYY-MM-DD hh:mm:ss
      * 与えられた値がYYYY-MM-DD hh:mm:ss形式であるかどうかを検証します
